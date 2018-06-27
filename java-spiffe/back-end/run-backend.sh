6b7cb03b6b96eae7f3ba1e411cf864bb81fbaec3
Max Lambrecht <maxlambrecht@gmail.com>
2018-06-27 11:28:13 -0300

    Minor changes to the java-spiffe example
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- java-spiffe/back-end/run-backend.sh
+++ java-spiffe/back-end/run-backend.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 su -c 'java -Dspiffe.endpoint.socket=/tmp/agent.sock \
             -Dtrusted.services.list=spiffe-id-whitelist \
-            -jar back-end.jar' backend
+            -jar back-end.jar' backend &
 
