ae35981aca0b03392f42ac2e3cf88b10d6223b16
Max Lambrecht <maxlambrecht@gmail.com>
2018-10-09 16:23:30 -0300

    Setting Envoy image version to tagged v1.8.0
    The Envoy container was updated to use the tagged version v1.8.0
    Envoy configuration files were adapted to comply with v1.8.0
    Tomcat release link was updated to point to gcloud storage
    Tomcat ports configuration broken in last commit was amended.
    
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- spiffe-envoy-sidecar/front-end/start-tomcat.sh
+++ spiffe-envoy-sidecar/front-end/start-tomcat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export JAVA_OPTS="$JAVA_OPTS -Dtasks.service=http://backend:8003/tasks/"
+export JAVA_OPTS="$JAVA_OPTS -Dtasks.service=http://localhost:8003/tasks/"
 /opt/tomcat/bin/catalina.sh run
