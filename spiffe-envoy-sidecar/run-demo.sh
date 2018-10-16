fd4fe00e999e7a434f7f58da31e77ee72bf1863a
Max Lambrecht <maxlambrecht@gmail.com>
2018-10-16 15:23:55 -0300

    Add absolute path to config files to simplify demo execution
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- spiffe-envoy-sidecar/run-demo.sh
+++ spiffe-envoy-sidecar/run-demo.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Needed to use --workdir option in docker-compose exec
-export COMPOSE_API_VERSION=1.35
-
 docker-compose exec -d spire-server ./spire-server run
 sleep 1
 echo "Spire Server is running"
@@ -14,7 +11,7 @@ echo "Entries Created"
 OUTPUT1=$(docker-compose exec spire-server ./spire-server token generate -spiffeID spiffe://example.org/host1 2>&1)
 TOKEN1=$( echo ${OUTPUT1} | cut -c8-43)
 
-docker-compose exec -d --workdir /opt/spire backend ./spire-agent run -joinToken $(echo $TOKEN1)
+docker-compose exec -d backend ./spire-agent run -joinToken $(echo $TOKEN1)
 sleep 1
 echo "Spire Agent 1 is running"
 
@@ -22,7 +19,7 @@ echo "Spire Agent 1 is running"
 OUTPUT2=$(docker-compose exec spire-server ./spire-server token generate -spiffeID spiffe://example.org/host2 2>&1)
 TOKEN2=$( echo ${OUTPUT2} | cut -c8-43)
 
-docker-compose exec -d --workdir /opt/spire frontend ./spire-agent run -joinToken $(echo $TOKEN2)
+docker-compose exec -d frontend ./spire-agent run -joinToken $(echo $TOKEN2)
 sleep 1
 echo "Spire Agent 2 is running"
 
